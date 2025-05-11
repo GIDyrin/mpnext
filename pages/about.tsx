d7919841d8ea2fd61246e0dfde0b1ac6cc63d081
GIDyrin <dyrin.glebka@gmail.com>
2025-05-12 00:20:27 +0300

    fix(about.tsx): Fixed layout

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,11 @@ import { AppLayout, Welcome } from "@/modules";
 export default function About(){
   return(
     <AppLayout>
-      <Welcome />
+      <div className="w-full max-w-4xl px-4">
+        <div className="text-center">
+          <Welcome />
+        </div>
+      </div>
     </AppLayout>
   )
 }
